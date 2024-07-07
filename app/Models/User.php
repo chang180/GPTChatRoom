c98091aed69805e3e1f50c6b99354a11b7d632a8
chang180 <chang180@gmail.com>
2024-07-07 20:26:13 +0800

    單人聊天室模式完成

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,4 +62,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
--- resources/js/Pages/ChatRoom.vue
+++ resources/js/Pages/ChatRoom.vue
@@ -1,27 +1,23 @@
-<script>
-import AppLayout from '@/Layouts/AppLayout.vue';
-</script>
-
 <template>
     <AppLayout title="Chat Room">
         <div class="flex flex-col h-screen">
-            <h1 class="text-2xl font-bold p-4 bg-blue-600 text-white">Chat Room</h1>
+            <h1 class="p-4 text-2xl font-bold text-white bg-blue-600">Chat Room</h1>
             <div id="messages" class="flex-1 p-4 overflow-y-auto bg-gray-100">
                 <div v-for="(message, index) in messages" :key="index" class="mb-2">
                     <strong>{{ message.user.name }}:</strong> {{ message.text }}
-                    <span class="text-gray-500 text-xs ml-2">{{ message.created_at }}</span>
+                    <span class="ml-2 text-xs text-gray-500">{{ message.created_at }}</span>
                 </div>
             </div>
-            <div class="p-4 bg-white flex">
+            <div class="flex p-4 bg-white">
                 <input
                     v-model="newMessage"
                     @keyup.enter="sendMessage"
                     placeholder="Type a message"
-                    class="flex-1 border p-2 rounded"
+                    class="flex-1 p-2 border rounded"
                 />
                 <button
                     @click="sendMessage"
-                    class="ml-2 p-2 bg-blue-600 text-white rounded"
+                    class="p-2 ml-2 text-white bg-blue-600 rounded"
                 >
                     Send
                 </button>
@@ -32,12 +28,17 @@ import AppLayout from '@/Layouts/AppLayout.vue';
 
 <script setup>
 import { ref, onMounted } from 'vue';
-import { usePage } from '@inertiajs/inertia-vue3';
+import { usePage } from '@inertiajs/vue3';
 import axios from 'axios';
 import { route } from 'ziggy-js';
+import AppLayout from '@/Layouts/AppLayout.vue';
 
-// 获取页面属性
-const { props } = usePage();
+// 定义接收的属性
+const props = defineProps({
+    messages: Array,
+    user: Object,
+});
+
 const messages = ref(props.messages || []);
 const newMessage = ref('');
 const user = ref(props.user);
